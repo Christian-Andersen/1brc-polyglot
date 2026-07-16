bb411b7695c64993385091cda5fc985f4fea6c78
Christian Andersen <cdandersen98@gmail.com>
2026-07-16 22:10:23 +1000

    added git and prek to flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          just
-          jdk21
-        ];
-      };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        prek
+        just
+        jdk21
+      ];
     };
+  };
 }
