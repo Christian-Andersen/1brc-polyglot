91316f9185c2c340acfff8445f467a7332fa2332
Christian Andersen <cdandersen98@gmail.com>
2026-07-17 21:03:32 +1000

    made a typescript solution

---
+++ solutions/typescript/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Frontend development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.permittedInsecurePackages = ["pnpm-10.34.0"];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        just
+        nodejs
+        prek
+      ];
+    };
+  };
+}
