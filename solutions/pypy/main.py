7c000e3a9d903225a5e18b8d3107e4668e9282fa
Christian Andersen <cdandersen98@gmail.com>
2026-07-17 16:44:30 +1000

    python implemention works and stuff

--- solutions/pypy/main.py
+++ solutions/pypy/main.py
@@ -1,19 +1,37 @@
+import math
 from collections import defaultdict
 from pathlib import Path
-from statistics import mean
+from typing import NamedTuple
 
 DATA_PATH = Path("../../data/measurements.txt")
 
 
+class Stats(NamedTuple):
+    min: int = 1000
+    max: int = -1000
+    total: int = 0
+    count: int = 0
+
+
+def round_toward_positive(value: float) -> float:
+    return math.floor(value + 0.5) / 10.0
+
+
 def main() -> int:
-    data = defaultdict(list)
+    data = defaultdict(Stats)
     with DATA_PATH.open("r") as f:
         for line in f:
             city, temp = line.strip().split(";")
-            data[city].append(float(temp))
+            temp = int(temp.replace(".", ""))
+            data[city] = Stats(
+                min=min(data[city].min, temp),
+                max=max(data[city].max, temp),
+                total=data[city].total + temp,
+                count=data[city].count + 1,
+            )
     s = "{"
     for city in sorted(data):
-        s += f"{city}={min(data[city]):.1f}/{mean(data[city]):.1f}/{max(data[city]):.1f}, "
+        s += f"{city}={round_toward_positive(data[city].min)}/{round_toward_positive(data[city].total / data[city].count)}/{round_toward_positive(data[city].max)}, "  # noqa: E501
     s = s[:-2] + "}"
     print(s)
     return 0
