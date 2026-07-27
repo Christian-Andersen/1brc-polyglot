7dbe343028ea1ff13508e2885c5a47af93fa8b27
Christian Andersen <christian.andersen@homeaffairs.gov.au>
2026-07-27 16:53:42 +1000

    added a lua solution

--- flake.nix
+++ flake.nix
@@ -26,6 +26,9 @@
         ruff
         ty
         rustfmt
+        luajit
+        stylua
+        selene
       ];
     };
   };
---
+++ solutions/lua/main.lua
@@ -0,0 +1,56 @@
+local function round_toward_positive(value)
+	return math.floor(value + 0.5) / 10.0
+end
+
+local MIN = 1
+local MAX = 2
+local TOTAL = 3
+local COUNT = 4
+
+local file, err = io.open("../../data/measurements.txt", "r")
+if not file then
+	error("Could not open file! Reason: " .. tostring(err))
+end
+
+local DATA = {}
+for line in file:lines() do
+	local city, temp = string.match(line, "([^;]+);([^;]+)")
+	temp = tonumber((string.gsub(temp, "%.", "", 1))) or error("Malformed line")
+	local stats = DATA[city]
+	if not stats then
+		stats = { temp, temp, temp, 1 }
+		DATA[city] = stats
+	else
+		if temp < stats[MIN] then
+			stats[MIN] = temp
+		end
+		if temp > stats[MAX] then
+			stats[MAX] = temp
+		end
+		stats[TOTAL] = stats[TOTAL] + temp
+		stats[COUNT] = stats[COUNT] + 1
+	end
+end
+file:close()
+
+local cities = {}
+for k in pairs(DATA) do
+	cities[#cities + 1] = k
+end
+table.sort(cities)
+print("{")
+for i, city in ipairs(cities) do
+	io.write(
+		string.format(
+			"%s=%.1f/%.1f/%.1f",
+			city,
+			round_toward_positive(DATA[city][MIN]),
+			round_toward_positive(DATA[city][TOTAL] / DATA[city][COUNT]),
+			round_toward_positive(DATA[city][MAX])
+		)
+	)
+	if i ~= #cities then
+		io.write(", ")
+	end
+end
+print("}")
