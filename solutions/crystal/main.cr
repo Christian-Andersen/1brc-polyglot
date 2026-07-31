d7cce26b6a3190de0942fe24ef50f7b575af8b59
Christian Andersen <cdandersen98@gmail.com>
2026-07-31 10:31:51 +1000

    formatted crystal solution

--- solutions/crystal/main.cr
+++ solutions/crystal/main.cr
@@ -1,17 +1,20 @@
 class Stats
-    property min : Int32
-    property max : Int32
-    property total : Int32
-    property count : Int32
-    def initialize(@min = Int32::MAX, @max = Int32::MIN, @total = 0, @count = 0)
-    end
-    def update(value : Int32)
-        @min = value if value < @min
-        @max = value if value > @max
-        @total += value
-        @count += 1
-    end
+  property min : Int32
+  property max : Int32
+  property total : Int32
+  property count : Int32
+
+  def initialize(@min = Int32::MAX, @max = Int32::MIN, @total = 0, @count = 0)
+  end
+
+  def update(value : Int32)
+    @min = value if value < @min
+    @max = value if value > @max
+    @total += value
+    @count += 1
+  end
 end
+
 data = {} of String => Stats
 File.each_line("../../data/measurements.txt") do |line|
   parts = line.split(";")
