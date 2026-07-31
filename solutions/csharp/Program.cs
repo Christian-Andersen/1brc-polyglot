25c52da992e85dbde4932b2cfc9330c2616984f7
Christian Andersen <cdandersen98@gmail.com>
2026-07-31 10:31:00 +1000

    got a csharp solution working

--- solutions/csharp/Program.cs
+++ solutions/csharp/Program.cs
@@ -1,1 +1,46 @@
-Console.WriteLine("Hello, World!");
+struct Stats
+{
+    public int Min { get; }
+    public int Max { get; }
+    public int Total { get; }
+    public int Count { get; }
+    public Stats(int min, int max, int total, int count)
+    {
+        Min = min;
+        Max = max;
+        Total = total;
+        Count = count;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var stats = new Dictionary<string, Stats>();
+        foreach (string line in File.ReadLines("../../data/measurements.txt"))
+        {
+            string[] parts = line.Split(";", 2);
+            string city = parts[0];
+            int.TryParse(parts[1].Replace(".", ""), out int temp);
+            if (stats.TryGetValue(city, out Stats existingStats))
+            {
+                stats[city] = new Stats(
+                    min: Math.Min(existingStats.Min, temp),
+                    max: Math.Max(existingStats.Max, temp),
+                    total: existingStats.Total + temp,
+                    count: existingStats.Count + 1
+                );
+            }
+            else
+            {
+                stats[city] = new Stats(temp, temp, temp, 1);
+            }
+        }
+        var sortedKeys = stats.Keys.OrderBy(k => k);
+        foreach (var key in sortedKeys)
+        {
+            Stats s = stats[key];
+            Console.WriteLine($"{key}\t{s.Min}\t{s.Max}\t{s.Total}\t{s.Count}");
+        }
+    }
+}
